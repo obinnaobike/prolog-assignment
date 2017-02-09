7a830b5a1eab8e642c3622598dd9109f77546f82
obinnaobike <obinna.obike@gmail.com>
2017-02-08 23:38:32 -0500

    thrid commit for problem 1

--- fp.pl
+++ fp.pl
@@ -1,10 +1,14 @@
 /* 1. */
-sum-up-numbers-simple(L, N).
-sum-up-numbers-simple([],0])
+/*sum-up-numbers-simple(L, N).*/
+/*sum-up-numbers-simple([],0])
 sum-up-numbers-simple([Head|Tail], N) :-
-sum-up-numbers-simple(Tail, Tails),
-N is Tail + Tails
+	sum-up-numbers-simple(Tail, Tails),
+	N is Tail + Tails.*/
 
+sum-up-numbers-simple([], 0).
+sum-up-numbers-simple([A|L], N) :-
+   sum-up-numbers-simple(L, Rest),
+   the_sum is L + Rest.
 
 
 
@@ -15,7 +19,5 @@ N is Tail + Tails
 /* 3 */
 
 
-
-/* 4 */
 
 
