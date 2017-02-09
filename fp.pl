0ed91c5df1ccaa78730272a5709c9e380fd5b27c
obinnaobike <obinna.obike@gmail.com>
2017-02-08 23:49:05 -0500

    commit for 2nd problem

--- fp.pl
+++ fp.pl
@@ -2,6 +2,9 @@
 
 sum-up-numbers-simple([], 0). /* base case if the list is empty return 0*/
 sum-up-numbers-simple([A|L], N) :- /* return the function recursively to add the tail end of the list */
+   number(N),
+   number(A),
+   number(L),
    sum-up-numbers-simple(L, B),
    the_sum is L + B.
 
@@ -9,6 +12,13 @@ sum-up-numbers-simple([A|L], N) :- /* return the function recursively to add the
 
 
 /* 2 */
+sum-up-numbers-general([], 0). /* base case if the list is empty return 0*/
+sum-up-numbers-general([A|L], N) :- /* return the function recursively to add the tail end of the list */
+   number(N),
+   number(A),
+   number(L),
+   sum-up-numbers-general(L, B),
+   the_sum is L + B.
 
 
 /* 3 */
