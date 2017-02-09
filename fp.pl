9ed2a177eb1f7b2b83a0a9c214c535b60b6f80f7
obinnaobike <obinna.obike@gmail.com>
2017-02-08 23:39:20 -0500

    fourth commit for problem 1

--- fp.pl
+++ fp.pl
@@ -1,14 +1,9 @@
 /* 1. */
-/*sum-up-numbers-simple(L, N).*/
-/*sum-up-numbers-simple([],0])
-sum-up-numbers-simple([Head|Tail], N) :-
-	sum-up-numbers-simple(Tail, Tails),
-	N is Tail + Tails.*/
 
-sum-up-numbers-simple([], 0).
+sum-up-numbers-simple([], 0). 
 sum-up-numbers-simple([A|L], N) :-
-   sum-up-numbers-simple(L, Rest),
-   the_sum is L + Rest.
+   sum-up-numbers-simple(L, B),
+   the_sum is L + B.
 
 
 
