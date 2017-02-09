58865b48fd311f3b874a64eaa3ba66789cd37eb1
obinnaobike <obinna.obike@gmail.com>
2017-02-08 23:40:17 -0500

    fifth commit for problem 1

--- fp.pl
+++ fp.pl
@@ -1,7 +1,7 @@
 /* 1. */
 
-sum-up-numbers-simple([], 0). 
-sum-up-numbers-simple([A|L], N) :-
+sum-up-numbers-simple([], 0). /* base case if the list is empty return 0*/
+sum-up-numbers-simple([A|L], N) :- /* return the function recursively to add the tail end of the list */
    sum-up-numbers-simple(L, B),
    the_sum is L + B.
 
